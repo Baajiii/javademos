a7901d44b10e39af8c2d9e12818fab41a0bcffee
Baajiii <samsudeenbasith00@gmail.com>
2022-07-13 11:02:07 +0530

    float and double examples

---
+++ floatAndDouble.java
@@ -0,0 +1,24 @@
+
+public class floatAndDouble{
+	public static void main(String[] args) {
+		int a = 6;
+		float b = 5.25f;
+		float c= 5.25f;
+		double d = 1255.252151;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		 float f = 7/2;
+		 System.out.println(f);
+		 float e = 7f/2f;
+		 System.out.println(e);
+		 float y = 7/2f;
+		 System.out.println(y);
+		 double k  = 7/2;
+		 System.out.println(k);
+		 double l  = 7/2d;
+		 System.out.println(l);
+		 System.out.println("a = " + a + ", b =" + b + ",c = " + c);
+	}
+}
